6f24fd6417a7d7b4f31736da7d70fd39c92a052f
Francis Salinger <francissalinger25@gmail.com>
2025-05-04 23:02:00 +0100

    Add 9 quiz questions

---
+++ main.py
@@ -0,0 +1,12 @@
+quiz_questions = [
+  {"question": "What is the capital of Canada?", "answer": "ottawa"},
+  {"question": "Which river flows through Cairo?", "answer": "nile"},
+  {"question": "What country has the most natural lakes?", "answer": "canada"},
+  {"question": "Who played Iron Man in the Marvel Cinematic Universe?", "answer": "robert downey jr"},
+  {"question": "What is the name of the Netflix series featuring a chess prodigy named Beth Harmon?", "answer": "the queen's gambit"},
+  {"question": "Which artist released the hit song 'Bad Guy' in 2019?", "answer": "billie eilish"},
+  {"question": "Who is the current president of the United States?", "answer": "donald trump"},
+  {"question": "Which country exited the European Union in 2020?", "answer": "united kingdom"},
+  {"question": "What international organization is abbreviated as 'UN'?", "answer": "united nations"}
+
+]
